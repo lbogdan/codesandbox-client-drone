b64b6edb33d045f9ef8f6ff60981a1aba8d3804c
Bogdan Luca <luca.bogdan@gmail.com>
2019-05-27 16:16:25 +0300

    Fix SSE URL on staging / PR builds.

--- packages/common/src/components/Preview/index.tsx
+++ packages/common/src/components/Preview/index.tsx
@@ -74,7 +74,7 @@ type State = {
 
 const getSSEUrl = (sandbox?: Sandbox, initialPath: string = '') =>
   `https://${sandbox ? `${sandbox.id}.` : ''}sse.${
-    process.env.NODE_ENV === 'development' ? 'codesandbox.io' : host()
+    (process.env.NODE_ENV === 'development' || process.env.STAGING) ? 'codesandbox.io' : host()
   }${initialPath}`;
 
 interface IModulesByPath {
